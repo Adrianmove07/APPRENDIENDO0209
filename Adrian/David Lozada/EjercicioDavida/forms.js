9861f032d537c465c747ea326c882dd7791f1081
unknown <Aprendiz@POPDFTCCSFSD002.FormacionSena.RED>
2023-03-08 18:00:54 -0500

    Adrian

---
+++ Adrian/David Lozada/EjercicioDavida/forms.js
@@ -0,0 +1,19 @@
+
+
+
+/*
+let enviarperfil =  ("click:", this.perfil); 
+
+console.log("enviarperfil:",  {nombreuser, apellidosuser, fechanacuser});
+
+function enviarperfil ("enviarperfil:", String){
+    nombre =this.nombreuser,
+    apellidos = this.apellidosuser,
+    fechanacimineto = this.fechanacuser
+}*/
+
+function enviarperfil (){
+    var nombreUsuario = document.getElementById(nombreuser).value;
+   
+    alert(nombreUsuario);
+}
